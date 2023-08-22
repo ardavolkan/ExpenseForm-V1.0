03438358debb4785076c22c310221b4302a82b31
Kerem Bıçak <138768566+bckkerem@users.noreply.github.com>
2023-08-22 17:32:18 +0300

    Entities Başlandı
    Entities Başlandı

---
+++ Entities/Concrete/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public string CategoryName { get; set; }
+        public string QuantityPerUnit { get; set; }
+
+
+    }
+}
