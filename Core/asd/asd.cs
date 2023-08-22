a03fd02517320729a6d149d8b2c64f2e882c50ef
ardavolkan <125757588+ardavolkan@users.noreply.github.com>
2023-08-22 16:41:25 +0300

    first commits

---
+++ Core/asd/asd.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.asd
+{
+    internal class asd
+    {
+    }
+}
---
+++ DataAccess/asd/asd.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.asd
+{
+    internal class asd
+    {
+    }
+}
