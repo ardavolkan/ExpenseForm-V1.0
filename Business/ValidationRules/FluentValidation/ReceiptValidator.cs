dcb19bde6166e296611cd7b41ad76392071ea211
ardavolkan <125757588+ardavolkan@users.noreply.github.com>
2023-08-23 16:58:21 +0300

    Autofac Eklendi
    Autofac Eklendi

---
+++ Business/ValidationRules/FluentValidation/ReceiptValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ReceiptValidator:AbstractValidator<Receipt>
+    {
+        public ReceiptValidator()
+        {
+            RuleFor(p => p.DocumentDate).NotEmpty();
+        }
+    }
+}
